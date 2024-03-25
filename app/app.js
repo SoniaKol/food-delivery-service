6c0a4b7234f1a259d1a0b02ed19ecc19e28d8cfe
Sofiia <sonia.nosenko@gmail.com>
2024-03-25 19:42:07 +0100

    finish

--- app/app.js
+++ app/app.js
@@ -11,3 +11,21 @@ document
   .addEventListener("click", function () {
     document.querySelector(".mobile-menu").classList.remove("is-open");
   });
+
+document.querySelector("#order-btn").addEventListener("click", function () {
+  document.querySelector(".backdrop").classList.toggle("is-open");
+});
+
+document
+  .querySelector("#hero-order-btn")
+  .addEventListener("click", function () {
+    document.querySelector(".backdrop").classList.toggle("is-open");
+  });
+
+document.querySelector("#offer-btn").addEventListener("click", function () {
+  document.querySelector(".backdrop").classList.toggle("is-open");
+});
+
+document.querySelector("#close-btn").addEventListener("click", function () {
+  document.querySelector(".backdrop").classList.remove("is-open");
+});
